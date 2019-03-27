1af867f4e305d22199fb08c7394f00a2e2596062
vahitevyapan <vahitevyapan@gmail.com>
2019-03-28 00:31:43 +0300

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reacttt
+            Learn React Coding
           </a>
         </header>
       </div>
